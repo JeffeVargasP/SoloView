2564bfe33e6aa63d466de362a412dcdeb2ccca5f
JeffeVargasP <jeffersonvargas745@gmail.com>
2024-10-20 20:37:41 -0300

    refactor: Add user update endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,7 @@ import { createUser } from "../controllers/user/create";
 import { loginUser } from "../controllers/user/login";
 import { findAll } from "../controllers/user/findAll";
 import { findOne } from "../controllers/user/findOne";
+import { updateUser } from "../controllers/user/update";
 
 export const userRoutes: Router = express.Router();
 
@@ -10,3 +11,4 @@ userRoutes.use("/", findAll);
 userRoutes.use("id/:id", findOne);
 userRoutes.use("/register", createUser);
 userRoutes.use("/login", loginUser);
+userRoutes.use("/update", updateUser);
